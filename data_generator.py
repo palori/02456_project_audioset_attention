55786d0ac439bb759baa34f3559716cf0952543c
palori <paulopezribas@gmail.com>
2018-11-18 20:20:27 +0100

    data_gen almost done, need to talk

--- data_generator.py
+++ data_generator.py
@@ -146,90 +146,148 @@ def read_h5_files(path, files, print_info=False): # Still to modify
             print('y',data[f]['y'].shape)
             print('video id',data[f]['video_id'].shape)
     return data #{'dict_data':data, 'pd_data':pd_data}
-    
-
-###################
-###################
-def gen_data():
-
-    #### START - GET DCASE LABELS ####
-    dcase_path = '../dcase2018_baseline/task4/dataset/metadata/'
-    dcase_files = get_dir_files(path=dcase_path, just_filename=False)
-
-    # Read data and add column with video_id from video name
-    dcase_data = read_csv_files(path=dcase_path, files=dcase_files, print_info=True)
-    
-    # get only strongly labeled data in a dict
-    dcase_str_lab_data = get_strongly_labeled(data=dcase_data, print_info=True)
-
-    #### END - GET DCASE LABELS ####
 
 
-    #### START - GET AUDIOSET DATA ####
-    audioset_path = '../packed_features/'
-    audioset_files = get_dir_files(path=audioset_path, just_filename=True, print_info=False)
-    # Keep just the ones with extension .h5 and NOT unbalanced data!
-    audioset_files = [f for f in audioset_files if (f.split('.')[-1] == 'h5') and (f.split('_')[0] != 'unbal')]
+def print_dict_shape(dict_data, title):
+	print('\n'+title+': ')
+	for k in dict_data.keys():
+		print("    "+k+":", dict_data[k].shape)
 
-    audioset_data = read_h5_files(path=audioset_path, files=audioset_files, print_info=True) # Still to modify
+def print_audioset_shape(audioset_data):
+	print('\nAudioset data:')
+	for kf in audioset_data.keys():
+		print('  '+kf+':')
+		for k in audioset_data[kf].keys():
+			print('    '+k+':', audioset_data[kf][k].shape)
 
-    # Let's have a look at some of the data
-    """
-    for d in data.keys():
-        print('\n\n'+d+':')
-        params = ['x','y','video_id_list']
-        for p in params:
-            print('\n'+p+':')
-            if p == 'video_id_list':
-                pass
-                print(data[d][p][0])
-            else:
-                print(data[d][p])
-    """
+##################
+##################
 
-    #### END - GET AUDIOSET DATA ####
+#### Dataset 0-initialization
+def dataset_init(x_shape, y_shape):
+	return {'x': np.zeros(x_shape),
+    		'y': np.zeros(y_shape),
+    		'video_id': np.array(['0'], dtype='bytes'),
+    		'onset': np.float32([0.0]),
+    		'offset': np.float32([0.0]),
+    		'event_label': np.array(['0'], dtype='bytes'),
+    		'filename': np.array(['0'], dtype='bytes')}
 
-
-
-    #### START - SELECTING STRONGLY LABELED DATA ####
-    
-    print('\n\nDCASE strongly labeled data: ', dcase_str_lab_data.keys())
-    print('Audioset data: ', audioset_data['bal_train.h5']['y'][0:1].shape)
-
+#### Find matches with 'video_id' and return the concatenation of them
+def concatenate_matches(dcase_str_lab_data, audioset_data):
+    # initialization
     x_shape = audioset_data['bal_train.h5']['x'][0:1].shape
     y_shape = audioset_data['bal_train.h5']['y'][0:1].shape
-
-    # initialization
-    dataset = {'x': np.zeros(x_shape),
-    		   'y': np.zeros(y_shape),
-    		   'video_id': np.array(['0'], dtype='bytes'),
-    		   'onset': np.float32([0.0]),
-    		   'offset': np.float32([0.0]),
-    		   'event_label': np.array(['0'], dtype='bytes'),
-    		   'filename': np.array(['0'], dtype='bytes')}
+    dataset = dataset_init(x_shape, y_shape)
                
-    print('\n\nDataset', dataset)
-    """
-    for 
-    found = False
-    for ka in audioset_data.keys(): # AudioSet
-		find_id = '9OqtuFGCCR8'#'Sb0169-lqLs'
-		#print(v.)
-		count = 0
-		for id_a in audioset_data[ka]['id']: # bal_train and eval
-		    if id_a == find_id:
-		        print(id_a)
-		        found = True
+    #print('\n\nDataset', dataset) # 0-initialization
+    
+    d_index = -1 # counter for dcase to be able to concatenate when match
+    for id_d in dcase_str_lab_data['video_id']: # ids DCASE
+    	found = False
+    	d_index = d_index + 1 
 
-		        dataset['x'] = np.concatenate(dataset['x'], [audioset_data[ka]['x'][count]])
-		        break
-		    count = count + 1
-		if found:
-			break
-    """
-    #### END - SELECTING STRONGLY LABELED DATA ####
+    	for ka in audioset_data.keys(): # AudioSet files
+    		a_index = -1 # counter for audioset to be able to concatenate when match
+    		
+    		for id_a in audioset_data[ka]['video_id']: # ids AudioSet
+    			a_index = a_index + 1 
+    			if id_a == id_d:
+    				found = True
+    				# Concatenate values
+    				#             from AudioSet
+    				for k in audioset_data[ka].keys():
+    					dataset[k] = np.concatenate((dataset[k], audioset_data[ka][k][a_index:a_index+1]))	
+    				#             from DCASE
+    				for k in dcase_str_lab_data.keys():
+    					if k != 'video_id':
+    						dataset[k] = np.concatenate((dataset[k], dcase_str_lab_data[k][d_index:d_index+1]))
 
+    				break
+    		if found:
+    			break
+    
+    return dataset
+
+###################
+###################
+def gen_data(dataset_file_name,
+	         dcase_path='../dcase2018_baseline/task4/dataset/metadata/',
+	         audioset_path='../packed_features/'):
+
+	#### START - SAVE IN 'H5' FILE ####
+    f = h5py.File(dataset_file_name, 'a') # read/create the file
+    file_exist = False
+    try:
+    	filekeys = [k for k in f['grp'].keys()]
+    	print('\n\nFile "'+dataset_file_name+'" exists')
+    	print('\nfilekeys:',filekeys)
+    	file_exist = True
+    except KeyError:
+    	print('\n\nInexistant file "'+dataset_file_name+'"')
+
+    if file_exist: # file not empty, proceed to read it	
+    	dataset = dataset_init((1, 10, 128), (1, 527)) #initialization with all needed keys
+    	for k in f['grp'].keys():
+    		if k != 'a_event_label' and k != 'a_filename': # @@@@temporal, need to be fixed!!!
+		    	print('\n    ... loading '+k+'...', type(dataset[k][0]))
+		    	d = 'grp/'+k
+		    	a = f.get(d)
+		    	if type(dataset[k][0]) != np.ndarray:
+		    		dataset[k] = np.array(a, dtype=type(dataset[k][0]))
+		    	else:
+		    		dataset[k] = np.array(a)
+
+    	print_dict_shape(dict_data=dataset, title='dataset shape')
+
+    else: # is an empty file, just created
+		#need to collect the data and save to the file
+
+	    #### START - GET DCASE LABELS ####
+	    dcase_files = get_dir_files(path=dcase_path, just_filename=False)
+
+	    # Read data and add column with video_id from video name
+	    dcase_data = read_csv_files(path=dcase_path, files=dcase_files, print_info=True)
+	    
+	    # get only strongly labeled data in a dict
+	    dcase_str_lab_data = get_strongly_labeled(data=dcase_data, print_info=True)
+	    #### END - GET DCASE LABELS ####
+
+
+	    #### START - GET AUDIOSET DATA ####
+	    audioset_files = get_dir_files(path=audioset_path, just_filename=True, print_info=False)
+	    # Keep just the ones with extension .h5 and NOT unbalanced data!
+	    audioset_files = [f for f in audioset_files if (f.split('.')[-1] == 'h5') and (f.split('_')[0] != 'unbal')]
+
+	    audioset_data = read_h5_files(path=audioset_path, files=audioset_files, print_info=True) # Still to modify
+	    #### END - GET AUDIOSET DATA ####
+
+
+
+	    #### START - SELECTING STRONGLY LABELED DATA ####
+	    print('\n\n----------------------------')
+	    print_dict_shape(dict_data=dcase_str_lab_data, title='DCASE data shape')
+	    print_audioset_shape(audioset_data)
+	    print('\n\n----------------------------')
+	    dataset = concatenate_matches(dcase_str_lab_data, audioset_data)
+	    print('\nMatches: ', len(dataset['video_id']), '/', len(dcase_str_lab_data['video_id']))
+	    print_dict_shape(dict_data=dataset, title='dataset shape')
+	    #### END - SELECTING STRONGLY LABELED DATA ####
+
+
+	    # Last step: save in the 'h5' file
+	    grp = f.create_group('grp')
+	    for k in dataset.keys():
+	    	print('\n  '+k+' type=', type(dataset[k][0]))
+	    	if k != 'event_label' and k != 'filename': # @@@@temporal, need to be fixed!!!
+		    	print('    ... saving '+k+'...')
+	    		grp.create_dataset(k, data=dataset[k])
+    
+    #### END - SAVE IN 'H5' FILE ####
     return dataset
 
 if __name__ == '__main__':
-    dataset = gen_data()
+	dataset_file_name = 'file_name_to_save_data.h5'
+	dcase_path = '../dcase2018_baseline/task4/dataset/metadata/'
+	audioset_path = '../packed_features/'
+	dataset = gen_data(dataset_file_name, dcase_path, audioset_path)
